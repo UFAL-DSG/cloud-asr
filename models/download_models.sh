616b747cceec9fb16f17a32d227805b6180d773a
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-06-23 23:12:22 +0200

    Script for models download added.

---
+++ models/download_models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/mfcc.conf
+wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mdl
+wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mat
+wget https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/HCLG_tri2b_bmmi.fst
+wget https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/words.txt
