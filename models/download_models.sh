9c3f28bc796f9f018c4dd0bc3b361a9f6c19d543
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-06-24 07:53:49 +0200

    Only newer models are downloaded.

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/mfcc.conf
-wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mdl
-wget https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mat
-wget https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/HCLG_tri2b_bmmi.fst
-wget https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/words.txt
+wget -NS https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/mfcc.conf
+wget -NS https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mdl
+wget -NS https://vystadial.ms.mff.cuni.cz/download/alex/resources/asr/voip_cs/kaldi/tri2b_bmmi.mat
+wget -NS https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/HCLG_tri2b_bmmi.fst
+wget -NS https://vystadial.ms.mff.cuni.cz/download/alex/applications/PublicTransportInfoCS/hclg/models/words.txt
