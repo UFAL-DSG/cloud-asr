a76aed147c4e51b528f8c08ca5c38ef30e1ebcd3
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-06-30 10:56:50 +0200

    OnlineASR uses batch ASR to get final hypothesis.

--- app/online_asr.py
+++ app/online_asr.py
@@ -1,19 +1,30 @@
+import wave
+import struct
+from StringIO import StringIO
+from background_asr import recognize_wav
+
 class OnlineASR:
     def __init__(self, emit):
         self.socket = 12345
         self.emit = emit
+        self.pcm = []
 
     def recognize_chunk(self, message):
+        for i in message['chunk']:
+            self.pcm.append(struct.pack('h', i))
+
         self.emit('result', {'result':[]})
 
     def end(self):
-        self.emit('result', {
-            "result": [
-                {
-                    "alternative": [{"transcript": "Hello World!", "confidence": 1.0}],
-                    "final": True,
-                },
-            ],
-            "result_index": 0
-        })
+        f = StringIO()
+        wav = wave.open(f, 'wb')
+        wav.setnchannels(1)
+        wav.setsampwidth(2)
+        wav.setframerate(44100)
+        wav.writeframes(b''.join(self.pcm))
+
+        self.emit('result', recognize_wav(f.getvalue()))
         self.emit('end', {})
+
+        wav.close()
+        f.close()
