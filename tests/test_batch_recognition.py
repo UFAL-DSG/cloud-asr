9b0343d5d5378f17405b6080f913c76d28a59716
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-09-03 19:01:41 +0200

    Failing acceptace test for batch recognition added.

---
+++ tests/test_batch_recognition.py
@@ -0,0 +1,65 @@
+import os
+import urllib2
+import StringIO
+import unittest
+import json
+from jsonschema import validate
+
+
+class TestBatchRecognition(unittest.TestCase):
+
+    @unittest.skip("not implemented yet")
+    def test_batch_recognition(self):
+        response = self.get_response_for_wav()
+
+        self.assertResponseHasCorrectSchema(response)
+
+    def get_response_for_wav(self):
+        url = "http://127.0.0.1:8000/recognize"
+        wav = self.load_wav()
+        headers = {"Content-Type": "audio/x-wav; rate=44100;"}
+        request = urllib2.Request(url, wav, headers)
+
+        return urllib2.urlopen(request).read()
+
+    def load_wav(self):
+        basedir = os.path.dirname(os.path.realpath(__file__))
+        return open("%s/../resources/test.wav" % basedir, "rb").read()
+
+    def assertResponseHasCorrectSchema(self, response):
+        schema = {
+            "type": "object",
+            "properties": {
+                "result": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "alternative": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "properties": {
+                                        "confidence": {"type": "number"},
+                                        "transcript": {"type": "string"},
+                                    },
+                                    "required": ["confidence", "transcript"],
+                                    "additionalProperties": False,
+                                },
+                                "minItems": 1,
+                            },
+                            "final": {"type": "boolean"},
+                        },
+                        "required": ["alternative", "final"],
+                        "additionalProperties": False,
+                    },
+                    "minItems": 1,
+                },
+                "result_index": {"type": "number"},
+            },
+            "required": ["result", "result_index"],
+            "additionalProperties": False,
+        }
+
+        validationResult = validate(json.loads(response), schema)
+        self.assertIsNone(validationResult, msg="Response has invalid schema")
