5177d8f9409e8d2846d424653c7929fd1c96fd03
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-10-28 22:28:39 +0100

    Probabilities from Kaldi are calibrated.

--- cloudasr/worker/asr.py
+++ cloudasr/worker/asr.py
@@ -1,22 +1,24 @@
-import math
+from math import *
 
 def create_asr():
     import config
     from kaldi.utils import lattice_to_nbest, wst2dict
     from kaldi.decoders import PyOnlineLatgenRecogniser
+    from asr_utils import lattice_calibration
 
     recogniser = PyOnlineLatgenRecogniser()
     recogniser.setup(config.kaldi_config)
     dictionary = wst2dict(config.wst_path)
 
-    return ASR(recogniser, dictionary, lattice_to_nbest)
+    return ASR(recogniser, dictionary, lattice_to_nbest, lattice_calibration)
 
 class ASR:
 
-    def __init__(self, recogniser, dictionary, lattice_to_nbest):
+    def __init__(self, recogniser, dictionary, lattice_to_nbest, lattice_calibration):
         self.recogniser = recogniser
         self.dictionary = dictionary
         self.lattice_to_nbest = lattice_to_nbest
+        self.lattice_calibration = lattice_calibration
 
     def recognize_chunk(self, chunk):
         decoded_frames = 0
@@ -33,9 +35,8 @@ class ASR:
         utt_lik, lat = self.recogniser.get_lattice()
         self.recogniser.reset()
 
-        return [(math.exp(-prob), self.path_to_text(path)) for (prob, path) in self.lattice_to_nbest(lat, n=10)]
+        return [(exp(-prob), self.path_to_text(path)) for (prob, path) in self.lattice_to_nbest(self.lattice_calibration(lat), n=10)]
 
     def path_to_text(self, path):
         return u' '.join([unicode(self.dictionary[w]) for w in path])
-
 
