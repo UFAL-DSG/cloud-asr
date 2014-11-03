fcc1eff3d053ea2ae857dd6c0f4cb608fb34ed3f
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-11-03 11:28:48 +0100

    Test fixed.

--- cloudasr/master/test_factory.py
+++ cloudasr/master/test_factory.py
@@ -4,5 +4,5 @@ from lib import create_master, Master
 class TestFactory(unittest.TestCase):
 
     def test_can_create_master(self):
-        master = create_master("ipc:///tmp/worker", "ipc:///tmp/frontend")
+        master = create_master("ipc:///tmp/worker", "ipc:///tmp/frontend", "ipc:///tmp/monitor/")
         self.assertIsInstance(master, Master)
