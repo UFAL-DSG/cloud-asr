f110a9c70f5cb85a0ac60cf2d36154121eaa7009
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-11-06 14:10:27 +0100

    Models are saved in /opt/models.

--- cloudasr/worker/models/download_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -NS https://vystadial.ms.mff.cuni.cz/download/pykaldi/egs/vystadial/online_demo/models_en/mfcc.conf
-wget -NS https://vystadial.ms.mff.cuni.cz/download/pykaldi/egs/vystadial/online_demo/models_en/tri2b_bmmi.mdl
-wget -NS https://vystadial.ms.mff.cuni.cz/download/pykaldi/egs/vystadial/online_demo/models_en/tri2b_bmmi.mat
-wget -NS https://vystadial.ms.mff.cuni.cz/download/pykaldi/egs/vystadial/online_demo/models_en/HCLG_tri2b_bmmi.fst
-wget -NS https://vystadial.ms.mff.cuni.cz/download/pykaldi/egs/vystadial/online_demo/models_en/words.txt
