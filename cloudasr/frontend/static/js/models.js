74573bbb63a9b1ac63fa8eb02a4a795e8d7ab397
Ondrej Klejch <ondrej.klejch@gmail.com>
2014-12-04 14:49:21 +0100

    Example worker with English TownInfo models created.

--- cloudasr/frontend/static/js/models.js
+++ cloudasr/frontend/static/js/models.js
@@ -1,5 +1,5 @@
 var models = {
-    "en-GB": "English (VYSTADIAL TownInfo AM+LM)",
+    "en-towninfo": "English (VYSTADIAL TownInfo AM+LM)",
     "en-wiki": "English (TED AM+Wikipedia LM)",
     "cs": "Czech (VYSTADIAL AM + Wikipedia LM)",
     "cs-alex": "Czech (VYSTADIAL AM + PTIcs LM)"
