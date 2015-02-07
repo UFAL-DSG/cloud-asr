0e368524222c9bfd0ab35bbda477b2a7cc61d5be
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-02-07 16:53:28 +0100

    Landing page and documentation added to web.

--- cloudasr/web/run.py
+++ cloudasr/web/run.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 app.secret_key = "12345"
 app.config['DEBUG'] = True
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/demo')
 def demo():
     return render_template('demo.html')
+
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
