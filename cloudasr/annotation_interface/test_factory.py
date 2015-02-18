3f084a185808eed2758660804b38295bff4679cf
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-02-18 20:07:00 +0100

    Integration tests fixed.

--- cloudasr/annotation_interface/test_factory.py
+++ cloudasr/annotation_interface/test_factory.py
@@ -1,10 +1,13 @@
 import unittest
 from lib import create_recordings_saver, create_db_connection, RecordingsSaver
+from cloudasr.models import WorkerTypesModel, RecordingsModel
 
 class TestFactory(unittest.TestCase):
 
     def test_can_create_recordings_saver(self):
-        db = create_db_connection("/tmp/db")
-        recordings_saver = create_recordings_saver("ipc:///tmp/worker", "/tmp/data", db)
+        db = create_db_connection("sqlite:////tmp/db")
+        worker_types_model = WorkerTypesModel(db)
+        recordings_model = RecordingsModel(db, worker_types_model, "/tmp/data", "localhost")
+        recordings_saver = create_recordings_saver("ipc:///tmp/worker", recordings_model)
         self.assertIsInstance(recordings_saver, RecordingsSaver)
 
