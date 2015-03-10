4248d87f556cebb98a85cd4bee268af2eb89f631
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-03-10 17:02:27 +0100

    Platform is stopped after unsuccessfull build.

--- deployment/jenkins/build.sh
+++ deployment/jenkins/build.sh
@@ -5,10 +5,11 @@ source ./env/bin/activate
 pip install -r requirements-pip.txt -b pip_build
 
 make build
-make run_locally && echo "Waiting for platform to start" && sleep 90
 make unit-test || exit 1
 make integration-test || exit 1
-make test || exit 1
+make run_locally && echo "Waiting for platform to start" && sleep 90
+make test || { make stop; exit 1; }
 make stop
+
 
 deactivate
