f9c39c27e4a8e88ccb12f95f1a56f26eed7e9814
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-03-19 23:10:31 +0100

    Sleep added to DummyWorker.

--- examples/worker_dummy/asr.py
+++ examples/worker_dummy/asr.py
@@ -1,3 +1,5 @@
+import time
+
 def create_asr():
     return DummyASR()
 
@@ -7,9 +9,11 @@ class DummyASR:
         pass
 
     def recognize_chunk(self, chunk):
+        time.sleep(len(chunk) / 16000 / 2)
         return (1.0, 'Dummy interim result')
 
     def get_final_hypothesis(self):
+        time.sleep(0.2)
         return [(1.0, 'Dummy final result')]
 
     def reset(self):
