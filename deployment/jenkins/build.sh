d1e29845836ff20f5dafaed82c1aa15efd5f5eed
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-05-06 22:18:27 +0200

    Build script fixed.

--- deployment/jenkins/build.sh
+++ deployment/jenkins/build.sh
@@ -7,7 +7,7 @@ pip install -r requirements-pip.txt -b pip_build
 make build || exit 1
 make unit-test || exit 1
 make integration-test || exit 1
-make run_locally && echo "Waiting for platform to start" && sleep 90
+make run && echo "Waiting for platform to start" && sleep 90
 make test || { make stop; exit 1; }
 make stop
 
